1e267724887a1cec8cdb2755c0886fb226f531d3
Yu Yang <yangyu1@lenovo.com>
2015-03-26 11:36:29 +0800

    getopt memo

---
+++ getopt/getopt.c
@@ -0,0 +1,170 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+
+void getopt_long_test(int argc, char **argv)
+{
+    int c;
+    int digit_optind = 0;
+
+    while (1) {
+        int this_option_optind = optind ? optind : 1;
+        int option_index = 0;
+        /*refer webbench*/
+        static struct option long_options[] = {
+            {"add", 1, 0, 0},
+            {"append", 0, 0, 0},
+            {"delete", 1, 0, 0},
+            {"verbose", 0, 0, 0},
+            {"create", 1, 0, 'c'},
+            {"file", 1, 0, 0},
+            {0, 0, 0, 0}
+        };
+
+        c = getopt_long(argc, argv, "abc:d:012",
+                long_options, &option_index);
+        if (c == -1)
+            break;
+
+        switch (c) {
+            case 0:
+                printf("option %s", long_options[option_index].name);
+                if (optarg)
+                    printf(" with arg %s", optarg);
+                printf("\n");
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+                if (digit_optind != 0 && digit_optind != this_option_optind)
+                    printf("digits occur in two different argv-elements.\n");
+                digit_optind = this_option_optind;
+                printf("option %c\n", c);
+                break;
+
+            case 'a':
+                printf("option a\n");
+                break;
+
+            case 'b':
+                printf("option b\n");
+                break;
+
+            case 'c':
+                printf("option c with value '%s'\n", optarg);
+                break;
+
+            case 'd':
+                printf("option d with value '%s'\n", optarg);
+                break;
+
+            case '?':
+                break;
+
+            default:
+                printf("?? getopt returned character code 0%o ??\n", c);
+        }
+    }
+
+    if (optind < argc) {
+        printf("non-option ARGV-elements: ");
+        while (optind < argc)
+            grintf("%s ", argv[optind++]);
+        printf("\n");
+    }
+}
+
+void getopt_gnu(int argc, char *argv[])
+{
+    int flags, opt;
+    int nsecs, tfnd;
+
+    nsecs = 0;
+    tfnd = 0;
+    flags = 0;
+
+    while ((opt = getopt(argc, argv, "-nt:")) != -1) {
+        switch (opt) {
+            case 'n':
+                flags = 1;
+                break;
+            case 't':
+                nsecs = atoi(optarg);
+                tfnd = 1;
+                break;
+            case 1:
+                printf("1 %s\n", optarg);
+                break;
+            default: /* '?' */
+                fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n",
+                        argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("flags=%d; tfnd=%d; optind=%d\n", flags, tfnd, optind);
+
+    if (optind >= argc) {
+        fprintf(stderr, "Expected argument after options\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("name argument = %s\n", argv[optind]);
+
+}
+
+
+void getopt_simple(int argc, char *argv[])
+{
+    int flags, opt;
+    int nsecs, tfnd;
+
+    nsecs = 0;
+    tfnd = 0;
+    flags = 0;
+
+    while ((opt = getopt(argc, argv, "nt:")) != -1) {
+        switch (opt) {
+            case 'n':
+                flags = 1;
+                break;
+            case 't':
+                nsecs = atoi(optarg);
+                tfnd = 1;
+                break;
+            default: /* '?' */
+                fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n",
+                        argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("flags=%d; tfnd=%d; optind=%d\n", flags, tfnd, optind);
+
+    if (optind >= argc) {
+        fprintf(stderr, "Expected argument after options\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("name argument = %s\n", argv[optind]);
+
+}
+
+int main(int argc, char *argv[])
+{
+    //opterr = 0;
+#if 0
+    getopt_simple(argc, argv);
+
+    optind = 0;
+
+    getopt_gnu(argc, argv);
+#endif
+
+    getopt_long_test(argc, argv);
+
+    exit(EXIT_SUCCESS);
+}
+
